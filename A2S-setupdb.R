edd5c1a6cd728717cbe6e05bc7c9e8cda13ee13a
rfr2126 <34722443+rfr2126@users.noreply.github.com>
2022-02-08 15:26:13 -0500

    Update A2S-setupdb.R

--- A2S-setupdb.R
+++ A2S-setupdb.R
@@ -1,12 +1,16 @@
-library(dplyr)
-#install.packages("RMySQL")
-install.
+# install.packages("dbplyr")
+# install.packages("DBI")
+# install.packages("RMySQL")
+# library(dbplyr)
+#library(dplyr)
+library(DBI)
 library(RMySQL)
-host="la-4051.chhrqrpvglpz.us-east-1.rds.amazonaws.com"
-port=3306
-dbname="la-4051"
-user="renatorusso"
-password="zcUbWvUWRNNN4pg"
 
-my_db = src_mysql(dbname=dbname, host=host, port=port, user=user, password=password)
+host <- "la-4051.chhrqrpvglpz.us-east-1.rds.amazonaws.com"
+port <- 3306
+dbname <- "la-4051"
+user <- "renatorusso"
+password <- "zcUbWvUWRNNN4pg"
 
+my_db = dbConnect(MySQL(), user = user, password = password, dbname = dbname, 
+                  host = host, port = port)
