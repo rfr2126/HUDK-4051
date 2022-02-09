555dc50d5e07ca86282f8479465201f21bbfa91b
renatorussobr <renrusso@gmail.com>
2022-02-09 06:44:39 -0500

    Update A2S-setupdb.R

--- A2S-setupdb.R
+++ A2S-setupdb.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # install.packages("dbplyr")
 # install.packages("DBI")
 # install.packages("RMySQL")
@@ -11,6 +12,22 @@ port <- 3306
 dbname <- "la-4051"
 user <- "renatorusso"
 password <- "zcUbWvUWRNNN4pg"
+=======
+library(dplyr)
+#install.packages("RMySQL")
+library(RMySQL)
+#install.packages("DBI")
+library(DBI)
+
+host = "la-4051.chhrqrpvglpz.us-east-1.rds.amazonaws.com"
+port = 3306
+dbname = "la-4051"
+user = "renatorusso"
+password = "zcUbWvUWRNNN4pg"
+
+my_db <- dbConnect(MySQL(), user = user, password = password, dbname = dbname, host = host, port = port)
+#  src_mysql(dbname = dbname, host = host, port = port, user = user, password = password)
+>>>>>>> Stashed changes
 
 my_db = dbConnect(MySQL(), user = user, password = password, dbname = dbname, 
                   host = host, port = port)
