faf631e65e0cabfa19772d94b0eae100a39addab
rfr2126 <34722443+rfr2126@users.noreply.github.com>
2022-02-18 09:11:45 -0500

    some code added

--- HDDK4051-bibliometric analysis.R
+++ HDDK4051-bibliometric analysis.R
@@ -3,3 +3,28 @@ file <- ("disinformation-references.bib")
 file
 View(file)
 M <- convert2df(file = file, dbsource = "isi", format = "bibtex")
+biblioshiny()
+
+View(M)
+
+results <- biblioAnalysis(M, sep = ";")
+results
+
+options(width=100)
+S <- summary(object = results, k = 10, pause = FALSE)
+
+plot(x = results, k = 10, pause = FALSE)
+
+#Authors’ Dominance ranking
+DF_dominance <- dominance(results, k = 10)
+DF_dominance
+
+
+#Create keyword co-occurrences network
+NetMatrix <- biblioNetwork(M, analysis = "co-occurrences", network = "keywords", sep = ";")
+
+#Plot the network
+net = networkPlot(NetMatrix, normalize="association", weighted=T, n = 30, Title = "Keyword Co-occurrences", type = "fruchterman", size=T,edgesize = 5,labelsize=0.7)
+
+#Conceptual Structure using keywords (method="CA")
+CS <- conceptualStructure(M,field="ID", method="CA", minDegree=4, clust=5, stemming=FALSE, labelsize=10, documents=10)
